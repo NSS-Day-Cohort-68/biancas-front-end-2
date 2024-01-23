fbbfc1b852ac70aeb09dd457e6cda46932b1419a
Eric Steedly <ericsteedly@gmail.com>
2024-01-23 14:25:02 -0600

    home page displays all bikes for user, or users own bikes

--- client/src/components/bikes/BikeList.js
+++ client/src/components/bikes/BikeList.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import BikeCard from "./BikeCard";
 import { getBikes } from "../../managers/bikeManager";
 
-export default function BikeList({ setDetailsBikeId }) {
+export default function BikeList({ setDetailsBikeId, loggedInUser }) {
   const [bikes, setBikes] = useState([]);
+  const [userBikes, setUserBikes] = useState([])
 
   const getAllBikes = () => {
     getBikes().then((res) => setBikes(res));
@@ -12,16 +13,34 @@ export default function BikeList({ setDetailsBikeId }) {
   useEffect(() => {
     getAllBikes();
   }, []);
+
+  useEffect(() => {
+    const uBikes = bikes.filter(bike =>
+      bike.userId === loggedInUser.id)
+    setUserBikes(uBikes)
+  }, [bikes, loggedInUser])
+
   return (
     <>
       <h2>Bikes</h2>
-      {bikes.map((bike) => (
+      {loggedInUser.isAdmin ?
+      bikes.map((bike) => (
         <BikeCard
           bike={bike}
           setDetailsBikeId={setDetailsBikeId}
           key={`bike-${bike.id}`}
         ></BikeCard>
-      ))}
+      ))
+      :
+      userBikes.map((bike) => (
+        <BikeCard
+          bike={bike}
+          setDetailsBikeId={setDetailsBikeId}
+          key={`bike-${bike.id}`}
+        >
+        </BikeCard>
+      ))
+      }
     </>
   );
 }
